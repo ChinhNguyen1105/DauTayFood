73d07778271d9ea483a48b917480ab62314847a9
ChinhNguyenGit <chinhnguyen.hp1105@gmail.com>
2025-07-04 11:41:15 +0700

    cap nhat header css

--- dautayfood-frontend/src/components/Header.jsx
+++ dautayfood-frontend/src/components/Header.jsx
@@ -2,33 +2,36 @@ import React from 'react';
 import './Header.css';
 import { FaShoppingCart, FaSearch, FaUserCircle } from 'react-icons/fa';
 import logo from '../assets/logo.png';
+
 const Header = () => {
     return (
-        <header className="header">
-            {/* Logo + tên thương hiệu */}
-            <div className="header-left">
-                <img src={logo} alt="Logo" className='logo' />
+        <header className="header_container">
+            <div id='header'>
+                <a href="#" rel='home' title='logo' className='logobox'>
+                    <img src={logo} alt="Logo" className='logo' />
+                </a>
+                <ul className='nav'>
+                    <li>
+                        <a href="#">Về chúng tôi</a>
+                    </li>
+                    <li>
+                        <a href="#">Giỏ hàng</a>
+                    </li>
+                    <li>
+                        <a href="#">Menu</a>
+                    </li>
+                </ul>
+                <div className="search_box">
+                    <input type="text" placeholder="Vietnamese stir fried morning glory...." />
+                    <button><FaSearch /></button>
+                </div>
+                {/* Đăng nhập */}
+                <div className="regist">
+                    <span>Đăng ký - Đăng nhập </span>
+                    <div id='avatar'> <FaUserCircle size={40} /></div>
+                </div>
             </div>
-
-            {/* Menu */}
-            <nav className="nav-links">
-                <a href="#">Về chúng tôi</a>
-                <a href="#">Giỏ hàng <FaShoppingCart /></a>
-                <a href="#">Menu</a>
-            </nav>
-
-            {/* Tìm kiếm */}
-            <div className="search-box">
-                <input type="text" placeholder="Vietnamese stir fried morning glory...." />
-                <button><FaSearch /></button>
-            </div>
-
-            {/* Đăng nhập */}
-            <div className="auth">
-                <span>Đăng ký - Đăng nhập</span>
-                <FaUserCircle size={20} />
-            </div>
-        </header>
+        </header >
     );
 };
 
